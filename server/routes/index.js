8562c840af52d0e27ce5ed5322a01b56a4121152
ashi6 <littletori0912@gmail.com>
2020-03-31 23:27:08 -0500

    initial user functionality

---
+++ server/routes/index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var fs = require("fs");
+var Account = require('../db/account');
+
+router.get('/', function(req, res) {
+	res.render('index', { user: req.user });
+});
+
+router.get('/register', function(req, res) {
+	res.render('register', { error: undefined });
+});
+
+router.post('/register', function(req, res, next) {
+	Account.register(new Account({ username: req.body.username.trim(), rooms: [] }), req.body.password.trim(), function(err, account) {
+		if (err) {
+			return res.render('register', { error: err.message });
+		}
+
+		passport.authenticate('local')(req, res, function() {
+			req.session.save(function(err) {
+				if (err) {
+					return next(err);
+				}
+				res.redirect('/');
+			});
+		});
+	});
+});
+
+router.get('/login', function(req, res) {
+	if (req.user) res.redirect('/');
+	else res.render('login', { user: req.user, error: req.query.err });
+});
+
+router.post('/login', function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res.render('login', { error: "Login Failed." });
+		}
+		req.logIn(user, function(err) {
+			if (err) { return next(err); }
+			return res.redirect('/');
+		});
+	})(req, res, next);
+});
+
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+router.get('/rooms', function(req, res) {
+	if (!req.user) {
+		res.render('nouser');
+		return;
+	}
+	res.render('rooms', { user: req.user });
+});
+
+router.get('/profile', function(req, res) {
+	if (!req.user) {
+		res.render('nouser');
+		return;
+	}
+	res.render('profile',  { user: req.user });
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,18 +2,42 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require("socket.io")(server);
+var cookieParser = require('cookie-parser');
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(cookieParser());
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+	extended: false
+}));
+
+app.use(require('express-session')({
+    secret: 's00per s3cr3t',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.render('index');
-});
+app.use(express.static('static'));
 
-app.use('/g', require('./routes/games'));
+app.use('/', require('./routes/index'));
 app.use('/r', require('./routes/rooms'));
 
-server.listen(3000, function() {
-    console.log(`Running on port 3000...`);
+var Account = require('./db/account');
+passport.use(Account.createStrategy());
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect('mongodb://localhost/zpy', { useNewUrlParser: true, useUnifiedTopology: true });
+
+server.listen(3003, function() {
+    console.log(`Running on port 3003...`);
 });
